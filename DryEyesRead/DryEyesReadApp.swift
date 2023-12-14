a918e1b15df9cb8e70ca3395693ff643c94dfdf8
PieterNatanael <pieternatanael@icloud.com>
2023-12-14 10:09:53 +0800

    Initial Commit

---
+++ DryEyesRead/DryEyesReadApp.swift
@@ -0,0 +1,17 @@
+//
+//  DryEyesReadApp.swift
+//  DryEyesRead
+//
+//  Created by Pieter Yoshua Natanael on 14/12/23.
+//
+
+import SwiftUI
+
+@main
+struct DryEyesReadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
